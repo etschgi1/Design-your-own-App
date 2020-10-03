7fee0b5774cf22422d6edcd81548c2f529610d8e
Elias Wachmann <elias.wachmann@gmail.com>
2020-10-03 10:03:16 +0200

    init

---
+++ pythonProject/Challenge-1_Wachmann_12004232.py
@@ -0,0 +1,16 @@
+################################################################################
+# Python challenge 1
+# Your task is to find out whether a certain input is a boolean, a string or a number below/above 12.
+#
+# Name: Wachmann Elias
+# Matriculation number: 12004232
+################################################################################
+
+# your code below here
+
+#take input from the user
+
+userin = input("Enter a text: ")
+
+userintype = type(userin)
+print(userin, userintype)
