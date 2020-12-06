47047ddf53ff021a208eb7f3045784f17d588b10
Elias Wachmann <elias.wachmann@gmail.com>
2020-12-06 11:02:18 +0100

    calc big O

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -1,1 +1,21 @@
-print(1 % 5)
+import math
+import time
+
+
+def printO(min, max):
+    print("Funktion: ")
+    k = 1
+    for n in range(min, max):
+        print(n)
+        # res = (7*k/3)*(n**(math.sqrt(math.pi)))  # first test
+        #res = n*n*math.log(10*n)-32*k*(1/(math.e**(n))+math.sqrt(42*n))
+        #res = (math.e**(math.sqrt((k)/(math.factorial(n)))))
+        timestart = time.time()
+        res = 0.35**(n+n**2)
+        print(f"{res} time: {time.time()-timestart}", end="")
+        timestart = time.time()
+        order = 0.35**(n)
+        print(f"{res}, time {time.time()-timestart}")
+
+
+printO(1000000000, 1000000010)
